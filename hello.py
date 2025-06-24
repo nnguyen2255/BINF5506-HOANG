249d7b0c1bdc14dabdb4e877b778e54637196ef4
Ubuntu <ubuntu@ip-172-31-81-171.ec2.internal>
2025-06-24 23:44:38 +0000

    add it!

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import os
 import boto3
  
+print("File upload starting")
+ 
 s3 = boto3.client("s3")
-bucket_name = "sohail-binf55062"
+bucket_name = "binf5506-hoang"
  
 s3.upload_file("hello.py", bucket_name, "hello.py")
  
